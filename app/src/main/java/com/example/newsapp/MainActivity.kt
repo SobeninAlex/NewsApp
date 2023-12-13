13f4fa3954df58fea52710966249e357aa26e693
SobeninAlex <sobeninalex@yandex.ru>
2023-12-13 15:45:15 +0500

    edit splash fragment

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(binding.root)
+            binding.bottomNavMenu.setupWithNavController(
+                navController = findNavController(R.id.nav_host_fragment)
+            )
+        }, 2000)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
